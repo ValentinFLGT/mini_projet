d048259648a92a360d9b8c98021156539789b49f
ValentinFLGT <valentin.folliguet@le-campu-numerique.fr>
2020-07-23 15:14:20 +0200

    FIX : Database - User Entity & Migration

--- migrations/Version20200723130243.php
+++ migrations/Version20200723130243.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200723121520 extends AbstractMigration
+final class Version20200723130243 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -20,15 +20,12 @@ final class Version20200723121520 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE SEQUENCE "user_id_seq" INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE TABLE "user" (id INT NOT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, role JSON NOT NULL, PRIMARY KEY(id))');
+
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('DROP SEQUENCE "user_id_seq" CASCADE');
-        $this->addSql('DROP TABLE "user"');
     }
 }
